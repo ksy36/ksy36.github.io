69b199bdf7edb0a4543253193ad87cff7235c0dd
Ksenia <kberezina@mozilla.com>
2023-04-21 12:39:36 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const cacheName = 'static-cache';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('Opened cache:', cacheName);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.endsWith('.png')) {
+        event.respondWith(
+            caches.match(event.request).then((cached) => {
+                // if (cached) {
+                //     return cached;
+                // }
+
+                return fetch(event.request).then((networkResponse) => {
+                    // Clone the response
+                    // const clonedResponse = networkResponse.clone();
+                    //
+                    // caches.open(cacheName).then((cache) => {
+                    //     cache.put(event.request, clonedResponse);
+                    // });
+
+                    return networkResponse;
+                });
+            })
+        );
+    } else {
+        event.respondWith(fetch(event.request));
+    }
+});
